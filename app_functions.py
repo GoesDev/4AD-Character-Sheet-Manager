196208a5d1256cd7dd1290ebb1f95c82b44f3749
Goes Dev <jcgoes.dev@gmail.com>
2024-11-07 21:29:43 -0300

    add select character functions

--- app_functions.py
+++ app_functions.py
@@ -1,4 +1,4 @@
-from db_functions import create_chracter
+from db_functions import save_character, select_character
 
 
 def new_character():
@@ -22,7 +22,34 @@ def new_character():
         '[Hand Weapon] Shortsword; [Armor] Hide Armor'
         )
 
-    create_chracter(new_chracter_data)
+    save_character(new_chracter_data)
 
 
-new_character()
+def select_existing_character(character_name):
+    """
+    Sends a character's name to a database query,
+    and adds its information to a dict.
+
+    Args:
+        character_name (string): name of an existing character in the database
+
+    Return:
+        character (dict): receives a tuple, transfers it to a dict, returns it
+    """
+
+    character_sheet = select_character(character_name)
+
+    character = {
+        "name": character_sheet[0],
+        "class": character_sheet[1],
+        "level": character_sheet[2],
+        "life": character_sheet[3],
+        "attack": character_sheet[4],
+        "defense": character_sheet[5],
+        "gold": character_sheet[6],
+        "special_abilities": character_sheet[7],
+        "clues": character_sheet[8],
+        "equipament": character_sheet[9]
+    }
+
+    return character
