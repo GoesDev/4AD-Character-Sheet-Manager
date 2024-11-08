2c01cc1701fc0a6e4658d340c47b2d7dc34514a8
Goes Dev <jcgoes.dev@gmail.com>
2024-11-07 21:30:07 -0300

    melhor legibilidade do código e adicionado novas funções

--- Character.py
+++ Character.py
@@ -5,29 +5,30 @@ class Character:
 
     def __init__(self, character):
         """
-        Cria o objeto de um novo personagem
+        Creates a new character object
 
-        Args (character (dict)) - dicionário com as informações do personagem.
+        Args (character (dict)) - dict with character information.
         """
 
-        self.name = character["name"]
-        self.character_class = character["class"]
-        self.level = character["level"]
-        self.life = character["life"]
-        self.attack = character["attack"]
-        self.defense = character["defense"]
-        self.gold = character["gold"]
-        self.special_abilities = character["special_abilities"]
-        self.special_abilities_uses = character["special_abilities_uses"]
-        self.clues = character["clues"]
-        self.equipament = character["equipament"]
+        self.name = character["name"].get()
+        self.character_class = character["class"].get()
+        self.level = character["level"].get()
+        self.life = character["life"].get()
+        self.attack = character["attack"].get()
+        self.defense = character["defense"].get()
+        self.gold = character["gold"].get()
+        self.special_abilities = character["special_abilities"].get()
+        self.special_abilities_uses = 0
+        self.clues = character["clues"].get()
+        self.equipament = character["equipament"].get()
 
     def save_new_character(self):
         """
-        Armazena as informações de um novo personagem no banco de dados
+        Stores a new character's information in the database
         """
 
-        # Cria a tupla que será passada para o banco em uma execução sql
+        # Creates a tuple that will be passed to the database
+        # in a SQL execution.
         new_character = (
             self.name,
             self.character_class,
@@ -44,19 +45,31 @@ class Character:
 
     def update_current_character(self, update_character_status):
         """
-        Altera as informações do personagem, e chama a função para dar um
-        UPDATE no banco de dados.
+        Changes the character information, and calls the function to perform an
+        UPDATE on the database.
         """
 
-        self.level = update_character_status["level"],
-        self.life = update_character_status["life"],
-        self.attack = update_character_status["attack"],
-        self.defense = update_character_status["defense"],
-        self.gold = update_character_status["gold"],
-        self.special_abilities = update_character_status["special_abilities"],
-        self.clues = update_character_status["clues"],
-        self.equipament = update_character_status["equipament"],
+        self.level = update_character_status["level"]
+        self.life = update_character_status["life"]
+        self.attack = update_character_status["attack"]
+        self.defense = update_character_status["defense"]
+        self.gold = update_character_status["gold"]
+        self.special_abilities = update_character_status["special_abilities"]
+        self.clues = update_character_status["clues"]
+        self.equipament = update_character_status["equipament"]
 
-        # Chamando a função que atualiza as informações no banco,
-        # enviado a tupla como parâmetro
-        update_character(update_character_status)
+        character_new_status = (
+            self.level,
+            self.life,
+            self.attack,
+            self.defense,
+            self.gold,
+            self.special_abilities,
+            self.clues,
+            self.equipament,
+            self.name
+        )
+
+        # Calling the function that updates the information in the Database,
+        # sent a tuple as a parameter.
+        update_character(character_new_status)
