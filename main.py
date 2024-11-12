5d8f150cd2137a40d61b83a872a6bf536faacfdb
Goes Dev <jcgoes.dev@gmail.com>
2024-11-12 14:25:48 -0300

    add 4 view character in Select Character window

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ all_class = ['Barbarian', 'Cleric', 'Dwarf',
              'Warrior', 'Wizard']
 root = tk.Tk()
 root.title('4AD - Character Sheet Manager')
-root.option_add("*Font", "Helvetica 10")
 
 style = ttk.Style()
-style.configure("TButton", font=("Helvetica", 11, "bold"))
+
+
+style.configure('TLabel', font=('Calibri', 13))
+style.configure("TButton", font=("Helvetica", 11, "bold"),
+                background='black', foreground='black', padding=10)
+
 
 global frm
 frm = ttk.Frame(root, padding=10)
@@ -38,19 +42,19 @@ def make_labels(ycol, xrow):
     ]
 
     for text, col, row in labels_texts:
-        ttk.Label(frm, text=text).grid(
+        ttk.Label(frm, text=text, style='TLabel').grid(
             column=col + ycol, row=row + xrow, sticky='E')
 
 
-def make_entries(ycol, xrow, character_dict, entries_dict_var):
+def make_entries(ycol, xrow, character_dict, character_dict_string_var):
     entries_items = [
-        ('name', 1, 2, entries_dict_var[0]),
-        ('level', 3, 2, entries_dict_var[2]),
-        ('life', 3, 3, entries_dict_var[3]),
-        ('attack', 1, 4, entries_dict_var[4]),
-        ('defense', 3, 4, entries_dict_var[5]),
-        ('clues', 1, 8, entries_dict_var[8]),
-        ('gold', 3, 8, entries_dict_var[9])
+        ('name', 1, 2, character_dict_string_var["name"]),
+        ('level', 3, 2, character_dict_string_var["level"]),
+        ('life', 3, 3, character_dict_string_var["life"]),
+        ('attack', 1, 4, character_dict_string_var["attack"]),
+        ('defense', 3, 4, character_dict_string_var["defense"]),
+        ('clues', 1, 8, character_dict_string_var["clues"]),
+        ('gold', 3, 8, character_dict_string_var["gold"])
     ]
 
     for key, col, row, var_string in entries_items:
@@ -58,9 +62,9 @@ def make_entries(ycol, xrow, character_dict, entries_dict_var):
         character_dict[key].grid(column=col + ycol, row=row + xrow, stick='W')
 
 
-def make_combobox(ycol, xrow, character_dict, entries_dict_var):
+def make_combobox(ycol, xrow, character_dict, character_dict_string_var):
     combo_items = [
-        ('class', 1, 3, entries_dict_var[1])
+        ('class', 1, 3, character_dict_string_var['class'])
     ]
     for key, col, row, var_string in combo_items:
         character_dict[key] = ttk.Combobox(frm,
@@ -80,6 +84,21 @@ def make_texts(ycol, xrow, character_dict):
         character_dict[key] = tk.Text(frm, width=55, height=3)
         character_dict[key].grid(column=col+ycol, sticky='W',
                                  row=row+xrow, columnspan=3)
+
+
+def make_dict_string_var():
+    return {
+        "name": tk.StringVar(),
+        "class": tk.StringVar(),
+        "level": tk.StringVar(),
+        "life": tk.StringVar(),
+        "attack": tk.StringVar(),
+        "defense": tk.StringVar(),
+        "gold": tk.StringVar(),
+        "special_abilities": tk.StringVar(),
+        "clues": tk.StringVar(),
+        "equipament": tk.StringVar()
+    }
 
 
 def window_new_character():
@@ -140,62 +159,66 @@ def window_delete_character():
 def window_show_party():
     clear_widgets()
     ttk.Label(frm, text="Party").grid(
-        column=1, row=0, columnspan=4)
+        column=2, row=0, columnspan=4)
     make_labels(0, 0)
+    make_labels(4, 0)
+    make_labels(0, 10)
+    make_labels(4, 10)
 
-    global test_dict, char_name, char_class, char_level, char_life
-    global char_gold, char_attack, char_defense, char_equipament, char_clues
-    global char_special_abilities, char_one
+    global char_one, char_one_dict, char_one_dict_string_var
+    global char_two, char_two_dict, char_two_dict_string_var
+    global char_three, char_three_dict, char_three_dict_string_var
+    global char_four, char_four_dict, char_four_dict_string_var
 
-    char_name = tk.StringVar()
-    char_class = tk.StringVar()
-    char_level = tk.StringVar()
-    char_life = tk.StringVar()
-    char_attack = tk.StringVar()
-    char_defense = tk.StringVar()
-    char_gold = tk.StringVar()
-    char_special_abilities = tk.StringVar()
-    char_clues = tk.StringVar()
-    char_equipament = tk.StringVar()
-
-    entries_dict_var = [
-        char_name,
-        char_class,
-        char_level,
-        char_life,
-        char_attack,
-        char_defense,
-        char_special_abilities,
-        char_equipament,
-        char_clues,
-        char_gold
-    ]
+    char_one_dict_string_var = make_dict_string_var()
+    char_two_dict_string_var = make_dict_string_var()
+    char_three_dict_string_var = make_dict_string_var()
+    char_four_dict_string_var = make_dict_string_var()
+    char_one_dict = {}
+    char_two_dict = {}
+    char_three_dict = {}
+    char_four_dict = {}
 
     ttk.Label(frm, text="#1 Character").grid(column=0, row=98)
     ttk.Label(frm, text="#2 Character").grid(column=2, row=98)
     ttk.Label(frm, text="#3 Character").grid(column=0, row=99)
     ttk.Label(frm, text="#4 Character").grid(column=2, row=99)
 
-    test_dict = {}
+    ttk.Label(frm).grid(column=0, row=9)
+    ttk.Label(frm).grid(column=0, row=10)
+    ttk.Label(frm).grid(column=0, row=20)
+    ttk.Label(frm).grid(column=0, row=21)
     char_one = ttk.Entry(frm)
-    char_one.grid(column=1, row=98)
+    char_one.grid(column=1, row=98, pady=15)
     char_two = ttk.Entry(frm)
-    char_two.grid(column=3, row=98)
+    char_two.grid(column=3, row=98, pady=15)
     char_three = ttk.Entry(frm)
-    char_three.grid(column=1, row=99)
+    char_three.grid(column=1, row=99, pady=15)
     char_four = ttk.Entry(frm)
-    char_four.grid(column=3, row=99)
+    char_four.grid(column=3, row=99, pady=15)
 
-    make_entries(0, 0, test_dict, entries_dict_var)
-    make_combobox(0, 0, test_dict, entries_dict_var)
-    make_texts(0, 0, test_dict)
+    make_entries(0, 0, char_one_dict, char_one_dict_string_var)
+    make_combobox(0, 0, char_one_dict, char_one_dict_string_var)
+    make_texts(0, 0, char_one_dict)
 
-    ttk.Button(frm, text='Select Characters', command=atualizar_entry).grid(
-        column=99, row=99)
+    make_entries(4, 0, char_two_dict, char_two_dict_string_var)
+    make_combobox(4, 0, char_two_dict, char_two_dict_string_var)
+    make_texts(4, 0, char_two_dict)
+
+    make_entries(0, 9, char_three_dict, char_three_dict_string_var)
+    make_combobox(0, 9, char_three_dict, char_three_dict_string_var)
+    make_texts(0, 9, char_three_dict)
+
+    make_entries(4, 9, char_four_dict, char_four_dict_string_var)
+    make_combobox(4, 9, char_four_dict, char_four_dict_string_var)
+    make_texts(4, 9, char_four_dict)
+
+    ttk.Button(frm, text='Select Characters', command=update_party_entry).grid(
+        column=7, row=99)
     ttk.Button(frm, text="Home",
-               command=window_home).grid(column=99, row=100)
+               command=window_home).grid(column=7, row=100)
     ttk.Button(frm, text="Quit",
-               command=root.destroy).grid(column=99, row=101)
+               command=root.destroy).grid(column=7, row=101)
 
 
 def window_home():
@@ -237,21 +260,39 @@ def delete_old_character():
     delete_character(character)
 
 
-def atualizar_entry():
+def update_character_entry(character_dict, character_number_dict, character):
 
-    character = select_character(char_one.get().title())
-    char_name.set(character[0])
-    char_class.set(character[1])
-    char_level.set(character[2])
-    char_life.set(character[3])
-    char_attack.set(character[4])
-    char_defense.set(character[5])
-    char_gold.set(character[6])
-    test_dict['special_abilities'].delete("1.0", tk.END)
-    test_dict['special_abilities'].insert(tk.END, character[7])
-    char_clues.set(character[8])
-    test_dict['equipament'].delete("1.0", tk.END)
-    test_dict['equipament'].insert(tk.END, character[9])
+    character_dict['name'].set(character[0])
+    character_dict['class'].set(character[1])
+    character_dict['level'].set(character[2])
+    character_dict['life'].set(character[3])
+    character_dict['attack'].set(character[4])
+    character_dict['defense'].set(character[5])
+    character_dict['gold'].set(character[6])
+    character_number_dict['special_abilities'].delete("1.0", tk.END)
+    character_number_dict['special_abilities'].insert(tk.END, character[7])
+    character_dict['clues'].set(character[8])
+    character_number_dict['equipament'].delete("1.0", tk.END)
+    character_number_dict['equipament'].insert(tk.END, character[9])
+
+
+def update_party_entry():
+
+    character_one = select_character(char_one.get().title())
+    update_character_entry(char_one_dict_string_var,
+                           char_one_dict, character_one)
+
+    character_two = select_character(char_two.get().title())
+    update_character_entry(char_two_dict_string_var,
+                           char_two_dict, character_two)
+
+    character_three = select_character(char_three.get().title())
+    update_character_entry(char_three_dict_string_var,
+                           char_three_dict, character_three)
+
+    character_four = select_character(char_four.get().title())
+    update_character_entry(char_four_dict_string_var,
+                           char_four_dict, character_four)
 
 
 window_home()
